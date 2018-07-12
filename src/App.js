bc7754ccc5f5ab86d176df4b7b8e09d35976d1ca
‘Michael <michael.daniels.jr@gmail.com>
2018-07-12 16:16:11 -0700

    title shortening

--- src/App.js
+++ src/App.js
@@ -12,15 +12,15 @@ class App extends Component {
     username: 'Michael',
     email: 'michael.daniels.jr@gmail.com',
     cards: [
-      {title:'Overboard', lyrics:''},
-      {title:'Your Girl', lyrics:''},
-      {title:'You\'re the Best', lyrics:''},
-      {title:'I\'m on One', lyrics:''},
-      {title:'Ball for Me', lyrics:''},
-      {title:'I Don\'t Want This', lyrics:''},
-      {title:'Mental Giant', lyrics:''},
-      {title:'Amazing', lyrics:''},
-      {title:'Top Notch', lyrics:''},
+      {title:'Overboard', lyrics:'porttitor rhoncus dolor purus non enim praesent elementum facilisis leo vel fringilla est ullamcorper eget nulla facilisi etiam dignissim diam'},
+      {title:'Your Girl', lyrics:'lacinia at quis risus sed vulputate odio ut enim blandit volutpat maecenas volutpat blandit aliquam etiam erat velit scelerisque in'},
+      {title:'You\'re the Best', lyrics:'vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum varius duis at'},
+      {title:'I\'m on One', lyrics:'eget gravida cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies leo integer malesuada'},
+      {title:'Ball for Me', lyrics:'quis imperdiet massa tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin libero nunc consequat interdum varius sit amet mattis'},
+      {title:'I Don\'t Want This', lyrics:'sed faucibus turpis in eu mi bibendum neque egestas congue quisque egestas diam in arcu cursus euismod quis viverra nibh'},
+      {title:'Mental Giant', lyrics:'habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas sed tempus urna et pharetra pharetra massa massa'},
+      {title:'Amazing', lyrics:'quam id leo in vitae turpis massa sed elementum tempus egestas sed sed risus pretium quam vulputate dignissim suspendisse in'},
+      {title:'Top Notch', lyrics:'sit amet nulla facilisi morbi tempus iaculis urna id volutpat lacus laoreet non curabitur gravida arcu ac tortor dignissim convallis'},
     ]
   }
 
--- src/SingleCard.js
+++ src/SingleCard.js
@@ -2,15 +2,19 @@ import React from 'react';
 
 
 const SingleCard = (props) => {
+  let slicedTitle = props.singlecard.title
+  if (props.singlecard.title.length > 15) {
+    slicedTitle = props.singlecard.title.slice(0, 15) + '...'
+  }
   return (
     <div className="col-md-3">
       <div className="single-card">
 
         <div>
-          <h4>{props.singlecard.title}</h4>
+          <h4>{slicedTitle}</h4>
         </div>
           <div>
-            <p>Lyrics here...</p>
+            <p>{(props.singlecard.lyrics).slice(0, 35)}...</p>
         </div>
 
       </div>
